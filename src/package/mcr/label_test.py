9cb2f9734499a971a1b0e98fea9a944ce33fecb1
Moritz Gottschling <moritz@giantswarm.io>
2023-09-01 13:12:21 +0200

    Add test for label

---
+++ src/package/mcr/label_test.py
@@ -0,0 +1,19 @@
+from package.mcr.label import IntermediateLabel, merge_intermediate_bags
+
+
+def test_merge_intermediate_bags():
+    il1 = IntermediateLabel([1, 1], [1, 1], [1, "a"], 1)
+    il2 = IntermediateLabel([2, 2], [2, 2], [2, "b"], 2)
+    il3 = IntermediateLabel([2, 1], [2, 1], [2, "c"], 3)
+
+    bag1 = [il1]
+    bag2 = [il2, il3]
+
+    merged_bag = merge_intermediate_bags(bag1, bag2)
+
+    assert (
+        len(merged_bag) == 2
+    )  # il1 and il3 should be in the merged bag, il2 is dominated by il1
+    assert il1 in merged_bag
+    assert il3 in merged_bag
+    assert il2 not in merged_bag
