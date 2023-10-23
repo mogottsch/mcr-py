1557194c3744705dedddcab823c5cfe1196cc8f8
Moritz Gottschling <moritz@giantswarm.io>
2023-10-23 14:33:17 +0200

    Refactor label calculation

--- src/rs/label.rs
+++ src/rs/label.rs
@@ -1,102 +1,77 @@
 use mlc::bag;
 
-const NEXT_BIKE_PRICE_INCREMENT_INTERVAL_TARIFF: u64 = 30; // minutes
-const NEXT_BIKE_PRICE_INCREMENT_INTERVAL_NO_TARIFF: u64 = 15; // minutes
-const NEXT_BIKE_PRICE_PER_INCREMENT_INTERVAL: u64 = 100; // cents
+struct PriceIncrementInfo {
+    interval: u64,
+    price_per_interval: u64,
+}
 
-const PERSONAL_CAR_PRICE_INCREMENT_INTERVAL: u64 = 1; // minutes
-const PERSONAL_CAR_PRICE_PER_INCREMENT_INTERVAL: u64 = 19; // cents
+fn calculate_price_increment_intervals(duration_minutes: u64, info: &PriceIncrementInfo) -> u64 {
+    duration_minutes / info.interval
+}
 
-pub fn next_bike_tariff(
+fn calculate_new_price(
     old_label: &bag::Label<usize>,
     new_label: &bag::Label<usize>,
+    info: &PriceIncrementInfo,
 ) -> bag::Label<usize> {
-    let old_hidden_values = old_label.hidden_values.clone();
-    let old_bicycle_duration = old_hidden_values[0];
-    let old_bicycle_duration_minutes = old_bicycle_duration / 60;
+    let old_duration_minutes = old_label.hidden_values[0] / 60;
+    let new_duration_minutes = new_label.hidden_values[0] / 60;
 
     let old_price_increment_intervals =
-        old_bicycle_duration_minutes / NEXT_BIKE_PRICE_INCREMENT_INTERVAL_TARIFF;
-
-    let new_hidden_values = new_label.hidden_values.clone();
-    let new_bicycle_duration = new_hidden_values[0];
-    let new_bicycle_duration_minutes = new_bicycle_duration / 60;
-
+        calculate_price_increment_intervals(old_duration_minutes, info);
     let new_price_increment_intervals =
-        new_bicycle_duration_minutes / NEXT_BIKE_PRICE_INCREMENT_INTERVAL_TARIFF;
+        calculate_price_increment_intervals(new_duration_minutes, info);
 
-    let price_increment = new_price_increment_intervals - old_price_increment_intervals;
-    let new_price = new_label.values[1] + price_increment * NEXT_BIKE_PRICE_PER_INCREMENT_INTERVAL;
+    let mut price_increment = new_price_increment_intervals - old_price_increment_intervals;
+
+    // Handle the case where the old duration is zero, and the new duration is not
+    if old_label.hidden_values[0] == 0 && new_label.hidden_values[0] != 0 {
+        price_increment = new_price_increment_intervals + 1;
+    }
+
+    let new_price = new_label.values[1] + price_increment * info.price_per_interval;
     let new_values = vec![new_label.values[0], new_price];
+
     bag::Label {
         node_id: new_label.node_id,
         path: new_label.path.clone(),
         values: new_values,
         hidden_values: new_label.hidden_values.clone(),
     }
+}
+
+const NEXT_BIKE_TARIFF_INFO: PriceIncrementInfo = PriceIncrementInfo {
+    interval: 30,
+    price_per_interval: 100,
+};
+
+const NEXT_BIKE_NO_TARIFF_INFO: PriceIncrementInfo = PriceIncrementInfo {
+    interval: 15,
+    price_per_interval: 100,
+};
+
+const PERSONAL_CAR_INFO: PriceIncrementInfo = PriceIncrementInfo {
+    interval: 1,
+    price_per_interval: 19,
+};
+
+pub fn next_bike_tariff(
+    old_label: &bag::Label<usize>,
+    new_label: &bag::Label<usize>,
+) -> bag::Label<usize> {
+    calculate_new_price(old_label, new_label, &NEXT_BIKE_TARIFF_INFO)
 }
 
 pub fn next_bike_without_tariff(
     old_label: &bag::Label<usize>,
     new_label: &bag::Label<usize>,
 ) -> bag::Label<usize> {
-    let old_hidden_values = old_label.hidden_values.clone();
-    let old_bicycle_duration = old_hidden_values[0];
-    let old_bicycle_duration_minutes = old_bicycle_duration / 60;
-
-    let old_price_increment_intervals =
-        old_bicycle_duration_minutes / NEXT_BIKE_PRICE_INCREMENT_INTERVAL_NO_TARIFF;
-
-    let new_hidden_values = new_label.hidden_values.clone();
-    let new_bicycle_duration = new_hidden_values[0];
-    let new_bicycle_duration_minutes = new_bicycle_duration / 60;
-
-    let new_price_increment_intervals =
-        new_bicycle_duration_minutes / NEXT_BIKE_PRICE_INCREMENT_INTERVAL_NO_TARIFF;
-
-    let mut price_increment = new_price_increment_intervals - old_price_increment_intervals;
-    if old_bicycle_duration == 0 && new_bicycle_duration != 0 {
-        price_increment = new_price_increment_intervals + 1;
-    }
-    let new_price = new_label.values[1] + price_increment * NEXT_BIKE_PRICE_PER_INCREMENT_INTERVAL;
-    let new_values = vec![new_label.values[0], new_price];
-    bag::Label {
-        node_id: new_label.node_id,
-        path: new_label.path.clone(),
-        values: new_values,
-        hidden_values: new_label.hidden_values.clone(),
-    }
+    calculate_new_price(old_label, new_label, &NEXT_BIKE_NO_TARIFF_INFO)
 }
 
 pub fn personal_car(
     old_label: &bag::Label<usize>,
     new_label: &bag::Label<usize>,
 ) -> bag::Label<usize> {
-    let old_hidden_values = old_label.hidden_values.clone();
-    let old_bicycle_duration = old_hidden_values[0];
-    let old_bicycle_duration_minutes = old_bicycle_duration / 60;
-
-    let old_price_increment_intervals =
-        old_bicycle_duration_minutes / PERSONAL_CAR_PRICE_INCREMENT_INTERVAL;
-
-    let new_hidden_values = new_label.hidden_values.clone();
-    let new_bicycle_duration = new_hidden_values[0];
-    let new_bicycle_duration_minutes = new_bicycle_duration / 60;
-
-    let new_price_increment_intervals =
-        new_bicycle_duration_minutes / PERSONAL_CAR_PRICE_INCREMENT_INTERVAL;
-
-    let mut price_increment = new_price_increment_intervals - old_price_increment_intervals;
-    if old_bicycle_duration == 0 && new_bicycle_duration != 0 {
-        price_increment = new_price_increment_intervals + 1;
-    }
-    let new_price =
-        new_label.values[1] + price_increment * PERSONAL_CAR_PRICE_PER_INCREMENT_INTERVAL;
-    let new_values = vec![new_label.values[0], new_price];
-    bag::Label {
-        node_id: new_label.node_id,
-        path: new_label.path.clone(),
-        values: new_values,
-        hidden_values: new_label.hidden_values.clone(),
-    }
+    calculate_new_price(old_label, new_label, &PERSONAL_CAR_INFO)
 }
